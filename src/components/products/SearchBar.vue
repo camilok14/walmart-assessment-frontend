9a139f470463f2f5c03d94372e95a2f31c81a253
Camilo Salazar <camilok14@gmail.com>
2020-12-01 20:24:11 -0300

    implements SearchBar component according to proposed unit tests

--- src/components/products/SearchBar.vue
+++ src/components/products/SearchBar.vue
@@ -1,11 +1,41 @@
 <template>
-  <div />
+  <div class="navbar">
+    <b-field>
+      <b-input v-model="auxValue" type="search" icon="magnify" :placeholder="label" rounded />
+    </b-field>
+  </div>
 </template>
 <script>
 export default {
-
+  props: {
+    label: { type: String, default: '' },
+    value: { type: String, default: '' }
+  },
+  data() {
+    return {
+      auxValue: ''
+    };
+  },
+  watch: {
+    value: {
+      immediate: true,
+      handler(newValue) {
+        this.auxValue = newValue;
+      }
+    },
+    auxValue: {
+      immediate: true,
+      handler(newValue) {
+        this.$emit('input', newValue);
+      }
+    }
+  }
 };
 </script>
 <style scoped>
-
+.navbar {
+  background-color: #126fca;
+  padding: 1% 0 1%;
+  justify-content: center;
+}
 </style>
